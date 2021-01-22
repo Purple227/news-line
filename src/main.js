b9c1c4e2157ba5e3223ee4ed79bd31f7149da52f
purple227 <psychogit227@gmail.com>
2021-01-22 22:30:09 +0100

    Set up development environment

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+Vue.use(VueAxios, axios)
+
+import "./style/app.scss";
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+//routes
+import routes from './routes';
+const router = new VueRouter
+({
+	mode: 'history',
+	routes
+});
+
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
---
+++ src/routes.js
@@ -0,0 +1,54 @@
+
+import Home from './components/Home.vue';
+
+const routes = 
+[
+
+{ 
+    name:'home',
+    path: '/',
+    component: Home
+},
+
+];
+
+export default routes;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
